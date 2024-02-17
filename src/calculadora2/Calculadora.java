fc5caac90931fb56fb90fd668696797433b7549b
Marcojr282 <marco.zapata282@gmail.com>
2024-02-16 19:54:19 -0500

    se soluciona todo

--- src/calculadora2/Calculadora.java
+++ src/calculadora2/Calculadora.java
@@ -6,6 +6,13 @@ public class Calculadora {
         super();
     }
     public static  Fraccion sumar(Fraccion fraccionUno, Fraccion fraccionDos){
+        if (fraccionUno == null){
+            throw new RuntimeException("La fraccion uno no puede estar vacia");
+        }
+        if (fraccionDos == null){
+            throw new RuntimeException("La fraccion dos no puede estar vacia");
+
+        }
         long numerador = fraccionDos.getNumerador() * fraccionDos.getDenominador()
                 + fraccionDos.getNumerador()* fraccionDos.getDenominador();
         long denominador = fraccionUno.getDenominador()* fraccionDos.getDenominador();
